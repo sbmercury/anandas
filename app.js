e9453b546d87d3964a06cf5e749951106b9d5e6d
spencerbartlett <spencerbartlett@vt.edu>
2020-04-26 18:44:48 -0400

    final fixes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 'use strict';
+const express = require('express');
 var app = express();
 app.use(express.static('public'));
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + '/index.html');
-}
+});
 
 
 
